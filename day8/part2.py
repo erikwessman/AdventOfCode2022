34d289b152b96b962152ae9c942dbb4f1a73032f
Erik Wessman <erikwessman10@gmail.com>
2022-12-12 15:59:14 +0100

    Cleaned up day 8

--- day8/part2.py
+++ day8/part2.py
@@ -6,41 +6,26 @@ arr = []
 for row in arr_input:
     arr.append([int(x) for x in row])
 
+def check_row(row, tree, start, stop, step):
+    result = 0
+    for i in range(start, stop, step):
+        val = row[i]
+        result += 1
+        if val >= tree:
+            break
+    return result
+
 def calc_scenic_score(x,y,w,h):
     tree = arr[y][x]
     row = arr[y]
     column = [r[x] for r in arr]
 
-    left = 0
-    for i in range(x-1, -1, -1):
-        val = row[i]
-        left += 1
-        if val >= tree:
-            break
-    
-    right = 0
-    for i in range(x+1, w):
-        val = row[i]
-        right += 1
-        if val >= tree:
-            break
+    l = check_row(row, tree, x-1,-1,-1)
+    r = check_row(row, tree, x+1, w, 1)
+    u = check_row(column, tree, y-1,-1,-1)
+    d = check_row(column, tree, y+1, h, 1)
 
-    up = 0
-    for i in range(y-1, -1, -1):
-        val = column[i]
-        up += 1
-        if val >= tree:
-            break
-
-    down = 0
-    for i in range(y+1, h):
-        val = column[i]
-        down += 1
-        if val >= tree:
-            break
-
-
-    return left*right*up*down
+    return l*r*u*d
 
 w,h = len(arr[0]), len(arr)
 result = 0
